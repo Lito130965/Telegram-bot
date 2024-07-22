94f7a2b93d108025ceaa2681853e1a769ac10bc1
lito130965 <lito130965@gmail.com>
2024-07-22 13:37:33 +0500

    Added dockerfile

--- telemembers/GetMembers.py
+++ telemembers/GetMembers.py
@@ -17,7 +17,7 @@ utils.get_peer_type = get_peer_type_new
 
 async def get_chat_members(chat_id) -> list:
     """ This function return all users tag, except bots """
-    app = Client('LTest', bot_token=bot_token)
+    app = Client('LTest', bot_token=bot_token, in_memory=True)
     chat_members = []
     await app.start()
     async for member in app.get_chat_members(chat_id=int(chat_id)):
